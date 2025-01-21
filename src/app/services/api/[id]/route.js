7b93a271ab2fda9c33bcaae1fa069c3d2b9cf502
ab-sid <absid24@gmail.com>
2025-01-21 16:35:37 +0600

    final code

--- src/app/services/api/[id]/route.js
+++ src/app/services/api/[id]/route.js
@@ -1,4 +1,5 @@
 import { connectDB } from "@/lib/connectDB";
+import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export const GET = async (request, { params }) => {
@@ -6,7 +7,7 @@ export const GET = async (request, { params }) => {
   const db = await connectDB();
   const servicesCollection = db.collection("services");
   try {
-    const res = await servicesCollection.findOne({ _id: id });
+    const res = await servicesCollection.findOne({ _id: new ObjectId(id) });
     return NextResponse.json(res);
   } catch (error) {
     return NextResponse.json({ message: "data not found" });
