37a60061f57beafadad22d195c43f56faa6ba8b7
ab-sid <absid24@gmail.com>
2025-01-21 17:05:26 +0600

    redirect problem2

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 
 export const middleware = async (request) => {
-  const token = request.cookies.get("__Secure-next-auth.session-token");
+  const token = request.cookies.get("next-auth.session-token");
   const pathname = request.nextUrl.pathname;
 
   if (pathname.includes("api")) {
